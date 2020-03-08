1551c8a3c7e4748ea1e39e7592e640300051cb6b
wfb <fubinsama@qq.com>
2020-03-08 19:03:26 +0800

    Initial commit

---
+++ s9_ehcache/src/test/java/demo/EhcacheApplicationTests.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EhcacheApplicationTests {
+
+    @Autowired
+    BookDao bookDao;
+
+    @Test
+    public void contextLoads() {
+        bookDao.getBookById1(1);
+        bookDao.getBookById1(1);
+        bookDao.getBookById2(2);
+        bookDao.getBookById2(2);
+        bookDao.deleteBookById(1);
+        Book b3 = bookDao.getBookById1(1);
+        System.out.println("b3: " + b3);
+        Book b = new Book();
+        b.setName("三国演义");
+        b.setAuthor("罗贯中");
+        b.setId(1);
+        bookDao.updateBookById(b);
+        Book b4 = bookDao.getBookById1(1);
+        System.out.println("b4: " + b);
+    }
+
+}
