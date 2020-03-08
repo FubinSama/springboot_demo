95319bf4f6fe2c06d2b20802ae3032113b967c31
wfb <fubinsama@qq.com>
2020-03-08 19:39:23 +0800

    m

---
+++ README.md
@@ -0,0 +1,12 @@
+# spring boot + vue全栈开发实战案例
+
+## S9：缓存
+
+### 9.1：Ehcache2.x缓存
+
+### 9.2：Redis单机缓存
+
+## S10：安全管理
+
+### 10.1：Spring Security的基本配置
+
---
+++ S9_redis/src/main/java/demo/BookDao.java
@@ -0,0 +1,47 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Component
+@CacheConfig(cacheNames = "c1")
+public class BookDao {
+    @Cacheable //默认使用方法的参数作为key，返回值作为value。一般加在查询方法上
+    //当缓存中有该数据时，不会执行方法，没有时执行方法，并将返回值缓存
+    public Book getBookById1(Integer id) {
+        System.out.println("getBookById1");
+        Book book = new Book();
+        book.setId(id);
+        book.setName("三国演义1");
+        book.setAuthor("罗贯中1");
+        return book;
+    }
+
+    @CachePut(key = "#book.id") //一般加载更新方法上，该方法一定会执行，并重新进行缓存
+    public Book updateBookById(Book book){
+        System.out.println("updateBookById");
+        book.setName("三国演义2");
+        return book;
+    }
+
+    @CacheEvict(key = "#id") //一般加载删除方法上，同时删除缓存中的该数据
+    public void deleteBookById(Integer id){
+        System.out.println("deleteBookById");
+    }
+
+    @Autowired
+    MyKeyGenerator myKeyGenerator;
+    @Cacheable(keyGenerator = "myKeyGenerator")
+    public Book getBookById2(Integer id) {
+        System.out.println("getBookById2");
+        Book book = new Book();
+        book.setId(id);
+        book.setName("三国演义2");
+        book.setAuthor("罗贯中2");
+        return book;
+    }
+}
